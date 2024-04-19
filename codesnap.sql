774dbfc912b32fea26577315cea7e28eb5cb79e8
Gonzalo <gonzalamocano@gmail.com>
2024-04-19 14:51:04 +0200

    BDD

---
+++ codesnap.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-04-2024 a las 14:44:15
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `codesnap`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `codesnap_users`
+--
+-- Error leyendo la estructura de la tabla codesnap.codesnap_users: #1932 - Table &#039;codesnap.codesnap_users&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla codesnap.codesnap_users: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `codesnap`.`codesnap_users`&#039; en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
